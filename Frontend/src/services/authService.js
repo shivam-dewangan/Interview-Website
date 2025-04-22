2fb599e9aed01a6313e7ffe3eb983662e9a3a13c
Shivam Dewangan <161064463+shivam-dewangan@users.noreply.github.com>
2025-04-22 20:00:48 +0530

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api/auth";
+const API_URL = "https://interview-website-n3ge.onrender.com/api/auth";
 
 // Signup function
 export const signup = async (userData) => {
